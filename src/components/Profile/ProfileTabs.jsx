88176bac5cba06b74250bd5bcc1f04569e2a82c2
Mohit Kumar 1004 <mohitkr741004@gmail.com>
2024-05-12 06:45:36 +0530

    Commit for Profile Page design with loading height intial

--- src/components/Profile/ProfileTabs.jsx
+++ src/components/Profile/ProfileTabs.jsx
@@ -1,8 +1,40 @@
+import { Box, Flex, Text } from "@chakra-ui/react"
+import { BsBookmark, BsGrid3X3, BsSuitHeart } from 'react-icons/bs'
+
 const ProfileTabs = () => {
   return (
-    <div>
-      Tabs
-    </div>
+    <Flex
+      w='full'
+      justifyContent='center'
+      gap={{ base: 4, sm: 10 }}
+      textTransform='uppercase'
+      fontWeight='bold'
+    >
+      <Flex borderTop='1px solid white' alignItems='center' p='3' gap={1} cursor='pointer'>
+        <Box fontSize={20}>
+          <BsGrid3X3 />
+        </Box>
+        <Text fontSize={12} display={{ base: 'none', sm: 'block' }} cursor='pointer'>
+          Posts
+        </Text>
+      </Flex>
+      <Flex alignItems='center' p='3' gap={1} cursor='pointer'>
+        <Box fontSize={20}>
+          <BsBookmark />
+        </Box>
+        <Text fontSize={12} display={{ base: 'none', sm: 'block' }} cursor='pointer'>
+          Saved
+        </Text>
+      </Flex>
+      <Flex alignItems='center' p='3' gap={1} cursor='pointer'>
+        <Box fontSize={20}>
+          <BsSuitHeart fontWeight='bold' />
+        </Box>
+        <Text fontSize={12} display={{ base: 'none', sm: 'block' }} cursor='pointer'>
+          Likes
+        </Text>
+      </Flex>
+    </Flex>
   )
 }
 
